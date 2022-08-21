3e57ebe782d18ba7ae94004d8970d4bd75c8d2c8
MashaMartin <59993828+MashaMartin@users.noreply.github.com>
2022-08-21 20:08:35 +0200

    Add files via upload

---
+++ src/ifStatement/ifStatementClass.java
@@ -0,0 +1,22 @@
+package ifStatement;
+
+public class ifStatementClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 8;
+		int b = 10;
+		if ( a > b) {
+			
+			System.out.println("The a is greater the b");
+		}else if(a < b) {
+			System.out.println("The a is less then b");
+			
+		}else {
+			System.out.println("The a is equal b");
+		}
+
+	}
+
+}
